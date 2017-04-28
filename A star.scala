5df547443a3d7cb733cd97058375cc356306948c
Inamdar <tinamdar@r-lnx228.cs.uiowa.edu>
2017-04-28 14:45:14 -0500

    Added State and A star methods

--- A star.scala
+++ A star.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.PriorityQueue
+
 def Astar (startState: State, goal: State, Operators: List[Operator], h: (State, State)=> Double): List[State] = {
   val frontier = PriorityQueue.empty[(Double, Int, State)](
     Ordering.by((_: (Double, Int, State))._1).reverse
@@ -6,11 +8,11 @@ def Astar (startState: State, goal: State, Operators: List[Operator], h: (State,
   while (!frontier.isEmpty) {
     val stateTuple = frontier.dequeue()
     val g = stateTuple._2
-    val state = stateTuple._3
+    var state = stateTuple._3
     if (state == goal) {
       var path = List[State]()
       while (state != startState) {
-        val prevState = state.predecessor()
+        val prevState = state.getPredecessor()
         path = state :: path
         state = prevState
       }  
@@ -19,12 +21,28 @@ def Astar (startState: State, goal: State, Operators: List[Operator], h: (State,
     state.setExplored()
     for (op <- Operators) {
         val output = op(state)
-        if (newState != None && !newState.get.isExplored()){
-          newState.get.setPredecessor(state)
-          frontier += ((g+1+h(newState.get, goalState), g+1, newState.get))
+        println(output)
+        if (output != None && !output.get.isExplored()){
+          output.get.setPredecessor(state)
+          frontier += ((g+1+h(output.get, goal), g+1, output.get))
         } 
       }
   }
   return List[State]()
 }
 
+def misplaced(a: BoardState, b: BoardState): Double = {
+  val amap = a.board.tiles.map(_.swap)
+  val bmap = b.board.tiles.map(_.swap)
+  var h = 0
+  for (i <- amap) {
+    var num = i._1
+    var diff1 = amap(num)._1 - bmap(num)._1
+    var diff2 = amap(num)._2 - bmap(num)._2
+    h += (if (diff1 > 0) diff1 else -diff1)
+    h += (if (diff2 > 0) diff2 else -diff2)
+  }
+  h
+}
+
+Astar(start, goalState(3), List(Left, Right, Up, Down), misplaced: (BoardState, BoardState) => Double)
---
+++ State.scala
@@ -0,0 +1,127 @@
+type Pos = (Int, Int)
+type Tile = Int
+
+case class Board( size: Int, tiles: Map[Pos,Tile] ) {
+
+  def swap(p1: Pos, p2: Pos) = {
+    val t1 = (tiles get p1, tiles get p2) match {
+      case (Some(v1), Some(v2)) => tiles + (p1 -> v2) + (p2 -> v1)
+      case (Some(v1), None    ) => (tiles - p1) + (p2 -> v1) 
+      case (None,     Some(v2)) => (tiles - p2) + (p1 -> v2) 
+      case _                    => tiles
+    }
+    new Board(size, t1)
+  } 
+
+  override def toString = {
+    var b = " +"
+    for (i <- 1 to size) 
+      b = b + "--"
+    b = b + "-+\n"
+  
+    var s = b
+    for (i <- 1 to size) {
+      s = s + " |" 
+      for (j <- 1 to size)
+        tiles get (i,j) match {
+          case None    => s = s + "  "
+          case Some(v) => s = s + " " + v
+        }
+      s = s + " |\n"  
+    }
+    s + b
+  } 
+}
+
+
+
+abstract class State() {
+  var explored: Boolean = false
+  var predecessor: State = null
+
+  def setExplored() = {
+    explored = !explored
+  }
+
+  def isExplored() = explored
+
+  def setPredecessor (s: State) = {
+    predecessor = s
+  }
+
+  def getPredecessor () = predecessor
+}
+
+case class BoardState (board: Board, emptyPos: Pos) extends State {
+  override def toString = board.toString + " " + emptyPos + "\n"
+}
+
+abstract class Operator {
+  def apply(s: State): Option[State]
+}
+
+
+case object Left extends Operator {
+  override def apply (s: State): Option[State] =
+  s match {
+    case BoardState(_, (_, 1)) => None
+    case BoardState(b, (r, c)) => {
+      val ep = (r, c - 1)
+      Some( BoardState(b.swap((r, c), ep), ep) )
+    }
+  }
+}
+
+case object Right extends Operator {
+  override def apply (s: State): Option[State] =
+  s match {
+    case BoardState(b, (r, c)) => if (c == b.size) None else {
+      val ep = (r, c + 1)
+      Some( BoardState(b.swap((r, c), ep), ep) )
+    }
+  }
+}
+
+case object Up extends Operator {
+  override def apply (s: State): Option[State] =
+  s match {
+    case BoardState(_, (1, _)) => None
+    case BoardState(b, (r, c)) => {
+      val ep = (r - 1, c)
+      Some( BoardState(b.swap((r, c), ep), ep) )
+    }
+  }
+}
+
+case object Down extends Operator {
+  override def apply (s: State): Option[State] =
+  s match {
+    case BoardState(b, (r, c)) => if (r == b.size) None else {
+      val ep = (r + 1, c)
+      Some( BoardState(b.swap((r, c), ep), ep) )
+    }
+  }
+}
+
+
+
+def goalState(n: Int) = {
+  var m: Map[Pos,Tile] = Map()
+  for (i <- 1 to n; j <- 1 to n) {
+    println((i,j))
+    m = m + ((i,j) -> (j + n * (i - 1)))
+  }
+  val lastPos = (n,n)
+  BoardState(Board(n, m - lastPos), lastPos)
+}
+
+val ts = Map(
+    (1,1) -> 1, (1,2) -> 2, (1,3) -> 3,
+    (2,1) -> 4,             (2,3) -> 7,
+    (3,1) -> 6, (3,2) -> 8, (3,3) -> 5
+    )
+
+// sample board
+val b = Board(3, ts)
+val start = BoardState(b, (2,2))
+
